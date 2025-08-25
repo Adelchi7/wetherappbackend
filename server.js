40a5a695125ac6d414fa5bd24a48ad5b75474e62
adelchi <adelchi.pelizzo@gmail.com>
2025-08-25 22:19:59 +0200

    correct url endpoint

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const API_URL = "https://wetherappbackend.onrender.com";
+
+fetch(`${API_URL}/wetherappbackend`)
+
 
 app.get("/ping", (req, res) => {
   res.json({ ok: true });
