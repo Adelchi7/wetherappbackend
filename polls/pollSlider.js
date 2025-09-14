865a32e7c1b2144caf8911e17008c57d57ef5158
adelchi <adelchi.pelizzo@gmail.com>
2025-09-14 19:13:49 +0200

    pollSlider hide loading

--- polls/pollSlider.js
+++ polls/pollSlider.js
@@ -109,7 +109,7 @@ function hideLoading() {
   const loader = document.getElementById("polls-loader");
   if (loader) {
     loader.classList.add("hidden");
-    setTimeout(() => loader.style.display = "none", 300);
+    setTimeout(() => loader.style.display = "none", 1000);
   }
 }
 
